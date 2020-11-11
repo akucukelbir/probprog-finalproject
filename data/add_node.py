9e805b0f0d67e5da44656109dfef1f8745cc601c
Asif Mallik <asif.mallik1@gmail.com>
2020-11-11 07:19:29 -0500

    Make add_node significantly faster by using k-d tree instead of OSMnx nearest node

--- data/add_node.py
+++ data/add_node.py
@@ -4,14 +4,35 @@ import numpy as np
 import pandas as pd
 import osmnx as ox
 from pandarallel import pandarallel
+from sklearn.neighbors import KDTree
 
+FILEPATH = 'reduced_data.csv'
 
 pandarallel.initialize()
 newyork_graph = ox.io.load_graphml('newyork.graphml')
 
+df = pd.read_csv(FILEPATH, index_col=0)
+
+#Load nodes from the map into K-d tree and dictionary
+points = np.zeros((len(newyork_graph.nodes),2))
+points_ID = {}
+for idx, node in enumerate(newyork_graph.nodes):
+    points[idx][0] = newyork_graph.nodes[node]['x']
+    points[idx][1] = newyork_graph.nodes[node]['y']
+    points_ID[tuple(points[idx])] = node
+tree = KDTree(points, leaf_size=2)
+
+b = df.shape[0]
+
 def populate_nearest_node(row):
-    point = (row['latitude'], row['longitude'])
-    return ox.distance.get_nearest_node(newyork_graph, point)
+    global b
+    b -= 1
+    if b % 100000 == 0:
+        print(b)
+    point = (row['longitude'], row['latitude'])
+    dist, ind = tree.query([point], k=1)
+    osmid = points_ID[tuple(points[ind[0][0]])]
+    return osmid
 
 df['node'] = df.parallel_apply(populate_nearest_node, axis=1)
 df.to_csv("reduced_node_data.csv", index=False)
