2da291c1458d35ff31dbf1fdb3a00ca7b9425a7b
Nicolas Beltran <nb2838@columbia.edu>
2020-11-15 22:13:50 -0500

    added script for shortest distance

--- data/data_utils.py
+++ data/data_utils.py
@@ -3,6 +3,8 @@ This file contains utility functions related to data cleaning
 or data processing that we use elsewhere
 """
 
+import numpy as np
+
 def lineseg_dists(p, a, b):
     """Cartesian distance from point to line segment
 
